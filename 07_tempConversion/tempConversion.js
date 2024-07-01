01e7552935c64fb89a446ee28776429089f7835e
Oscar Cañedo <oscar.r.canedo@gmail.com>
2024-07-01 21:19:51 +0200

    01 - JS TDD exercises: 06 - 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,41 @@
-const convertToCelsius = function() {
+/*   
+FUNCTION to convert from faranheit to celsius
+FORMULA (x °C ≘ (x × 9/5 + 32) °F)
+PARAMETER number (negative inclusive)
+*/
+const convertToCelsius = function (fahrenheit) {
+
+  // 1. CHECK for input type to be number only
+  // 2. ROUND number to one decimal place
+  // 3. CONVERT returned rounded string back to number
+  // 4. IMPLEMENT formula
+    // SUBTRACT 32 from Fahrenheit value
+    // MULTIPLY result by 5 / 9
+  // 5. RETURN result rounded to one decimal place
+
+  if (typeof fahrenheit !== "number") {
+    return "Input must be a number";
+  }
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+/*   
+FUNCTION to convert from celcius to faranheit    
+FORMULA (x °F ≘ (x − 32) × 5/9 °C)
+PARAMETER number (negative inclusive)
+*/
+const convertToFahrenheit = function (celsius) {
+
+  if (typeof celsius !== "number") {
+    return "Input must be a number";
+  }
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  //INITIALIZE sum
+  let sum = 0;
+
+  // LOOP through elements and add recursively to sum
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  // RETURN result
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  // REDUCE method called on array
+  // CALLBACK FUNCTION is passed to reduce
+  // INITIALIZE value 1 (multiplication to 0 == 0), passed as second argument to reduce
+  return arr.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  // FORMULA n! = n × (n - 1)
+  /* PATTERN: 
+    0! = 1
+    1! = 1
+    2! = 2 * 1
+    3! = 3 * 2 * 1
+    4! = 4 * 3 * 2 * 1
+    5! = 5 * 4 * 3 * 2 * 1
+ */
+
+  let result = num;
+  if (num === 0 || num === 1) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +56,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
